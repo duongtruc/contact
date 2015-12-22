0e8bd7b019576533ab556fb5f365d87aa43381ae
Duong Truc <duongtruc.92@gmail.com>
2015-12-22 17:08:59 +0700

    2

--- contact.php
+++ contact.php
@@ -1,23 +1,22 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$username='ndthelp@gmail.com';
-$token='bf760bb00bf5ae26c3114d05c33b312d8b573185';
-$URL='https://cstest.lvtn.xyz/api/requesters/search?email=duongtruc.92@gmail.com';
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$ROOT_URL='https://cstest.lvtn.xyz/api';
+$search_url = '/requesters/search?';
 $auth = "Authorization: Bearer ndthelp@gmail.com:bf760bb00bf5ae26c3114d05c33b312d8b573185";
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$URL);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array($auth));
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$ROOT_URL.$search_url.'email='.$email);
 $result = curl_exec ($ch);
-echo curl_getinfo($ch) . '<br/>';
-echo curl_errno($ch) . '<br/>';
-echo curl_error($ch) . '<br/>';
-var_dump($result);
+echo $result;
 curl_close ($ch);
 ?>
 <h3>
--- index.php
+++ index.php
@@ -13,34 +13,34 @@
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="fname" name="name" type="text" placeholder="First Name" class="form-control">
+                                <input id="fname" name="first_name" type="text" placeholder="First Name" class="form-control" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="lname" name="name" type="text" placeholder="Last Name" class="form-control">
+                                <input id="lname" name="last_name" type="text" placeholder="Last Name" class="form-control" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="email" name="email" type="text" placeholder="Email Address" class="form-control">
+                                <input id="email" name="email" type="email" placeholder="Email Address" class="form-control" required>
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <!--<div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
                             <div class="col-md-8">
                                 <input id="phone" name="phone" type="text" placeholder="Phone" class="form-control">
                             </div>
-                        </div>
+                        </div>-->
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <textarea class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
+                                <textarea required class="form-control" id="message" name="message" placeholder="Enter your massage for us here. We will get back to you within 2 business days." rows="7"></textarea>
                             </div>
                         </div>
 
