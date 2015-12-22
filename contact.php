22c781a50f1366bb90b69dda014a94bdcbf0fdb2
Duong Truc <duongtruc.92@gmail.com>
2015-12-22 16:34:05 +0700

    2

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$URL);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $auth));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $auth));
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);//get status code
 $result=curl_exec ($ch);
 var_dump($result);
