a5b3009ecd1e506a95c063891d8be588eaa79ebd
Duong Truc <duongtruc.92@gmail.com>
2015-12-22 16:24:43 +0700

    2

--- contact.php
+++ contact.php
@@ -1,5 +1,17 @@
 <?php
-    var_dump($_POST);
+$username='ndthelp@gmail.com';
+$token='bf760bb00bf5ae26c3114d05c33b312d8b573185';
+$URL='https://cstest.lvtn.xyz/api/requesters/search?email=duongtruc.92@gmail.com';
+$auth = "Authorization: Bearer ndthelp@gmail.com:bf760bb00bf5ae26c3114d05c33b312d8b573185";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$URL);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $auth));
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);//get status code
+$result=curl_exec ($ch);
+var_dump($result);
+curl_close ($ch);
 ?>
 <h3>
     Thanks for your infomation, we'll contact you back as soon as possible
