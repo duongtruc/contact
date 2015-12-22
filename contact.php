ee426fd9b157aceac67c8e5b82a8306177f7af26
Duong Truc <duongtruc.92@gmail.com>
2015-12-22 16:44:28 +0700

    2

--- contact.php
+++ contact.php
@@ -7,8 +7,6 @@ $URL='https://cstest.lvtn.xyz/api/requesters/search?email=duongtruc.92@gmail.com
 $auth = "Authorization: Bearer ndthelp@gmail.com:bf760bb00bf5ae26c3114d05c33b312d8b573185";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$URL);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array($auth));
 $result = curl_exec ($ch);
 var_dump($result);
