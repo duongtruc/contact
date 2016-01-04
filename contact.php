484702c43434a36f2cd81aa16d6d226098106687
Duong Truc <duongtruc.92@gmail.com>
2016-01-04 11:04:12 +0700

    2

--- contact.php
+++ contact.php
@@ -21,7 +21,6 @@ $result = curl_exec($ch);
 $result = json_decode($result, true);
 if ($result && count($result['data'])) {
     $requester = $result['data'][0];
-    var_dump($requester);
     $requester_id = $requester['_id'];
     $ticket = json_encode(array(
         'ticket'    => array(
@@ -34,6 +33,7 @@ if ($result && count($result['data'])) {
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_URL,$ROOT_URL.$ticket_url);
     $result = curl_exec($ch);
+    var_dump($result);
 } else {
     $requester = json_encode(array(
         'requester'    => array(
