b084d82b64769b70da152d1a8859506746804ecb
Duong Truc <duongtruc.92@gmail.com>
2016-01-04 10:52:57 +0700

    2

--- contact.php
+++ contact.php
@@ -61,7 +61,7 @@ if ($result && count($result['data'])) {
 }
 curl_close ($ch);
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <div class="container">
     <div class="row">
         <div class="col-md-12">
